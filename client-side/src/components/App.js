21cfe3bcf9287918fb119d81c524e10739cd46c9
Jacob <jacobamelse@hotmail.com>
2023-02-19 20:09:54 -0600

    fetched pokemon to app and added css for cards for a baseline

--- client-side/src/components/App.js
+++ client-side/src/components/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
 
 function App() {
+  const [pokemon, setPokemon] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:9292/pokemon')
+    .then(r => r.json())
+    .then(data => setPokemon(data))
+  }, [])
+
+  function capitalize(str){
+    return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="AppContainer">
+      {pokemon.map((poke) => {
+        return (
+        <div className='pokeCard'>
+          <div className={`cardImg${poke.poke_type}`}>
+            <img src={poke.sprite} alt="Pokemon Sprite"/>
+          </div>
+          <div className={`cardContent${poke.poke_type}`}>
+            <h1 className='cardName'>{capitalize(poke.name)}</h1>
+            <span className='cardType'>{capitalize(poke.poke_type)}</span>
+            <p className='cardDesc'>{poke.description}</p>
+            <h1 className='cardButton'><a href='#'>Placeholder Text</a></h1>
+          </div>
+        </div>
+        )})}
     </div>
   );
 }
