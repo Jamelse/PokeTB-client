2144ecdde5e1eeb0f139b0ce0f957afe6734d21d
Jacob <jacobamelse@hotmail.com>
2023-03-21 20:42:18 -0500

    changed some css styling on home page and created css for contact page

--- client-side/src/components/ContactPage.js
+++ client-side/src/components/ContactPage.js
@@ -2,10 +2,14 @@ import React from "react";
 
 function ContactPage(){
   return(
-    <div>
-      <a href="https://github.com/Jamelse" target="_blank"><h1>GitHub</h1></a>
-      <a href="https://linkedin.com/in/jacob-amelse-69116a255" target="_blank"><h1>LinkedIn</h1></a>
-      <h1>jacobamelse@hotmail.com</h1>
+    <div className="contactDiv">
+      <a className="contactLink" href="https://github.com/Jamelse" target="_blank"><h1>GitHub</h1></a>
+      <a className="contactLink" href="https://linkedin.com/in/jacob-amelse-69116a255" target="_blank"><h1>LinkedIn</h1></a>
+      <h1>Jacobamelse@hotmail.com</h1>
+      <br/>
+      <div className="contactRightsDiv">
+      <h3>© 2023 - PokeTB - All Rights Reserved</h3>
+      </div>
     </div>  
   )
 }
--- client-side/src/components/Home.js
+++ client-side/src/components/Home.js
@@ -8,10 +8,10 @@ return (
   <div className='homeDiv'>
     <div className='homeLogoTitle'>
       <img  className='homeLogo' src={logo} alt='logo'/>
-      <h1>Welcome to Poke Team Builder</h1>
+      <h1>Welcome to PokeTB</h1>
     </div>
     <h3 className='homeSubheader'>A pokemon team building tool to help keep your teams organized</h3>
-		<Link to='/trainers/new'><button className='homeButton'>Get Started</button></Link>
+		<Link className='homeButtonLink' to='/trainers/new'><button className='homeButton'>Get Started</button></Link>
 	</div>
 )
 }
